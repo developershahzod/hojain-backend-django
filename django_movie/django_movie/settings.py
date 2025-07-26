3a79c5a0fad3b4edd2f63b56bb2d97735c06fabc
Shahzod Akhmedov <shahzodakhmedov@MacBook-Pro-Shahzod.local>
2025-07-27 04:58:52 +0800

    sqlite-vers

--- django_movie/django_movie/settings.py
+++ django_movie/django_movie/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'xgl9fm1xdp0+nopt#!cm7^c)g!t0)0*uag$4y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['*']
 
 
 

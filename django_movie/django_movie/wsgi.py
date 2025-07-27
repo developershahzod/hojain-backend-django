1bb1fa9c290458e42ea7207d7612982b7c49672a
Shahzod Akhmedov <shahzodakhmedov@MacBook-Pro-Shahzod.local>
2025-07-27 15:01:35 +0800

    update wsgi

--- django_movie/django_movie/wsgi.py
+++ django_movie/django_movie/wsgi.py
@@ -10,7 +10,13 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_movie.settings')
+
+call_command('makemigrations')
+call_command('migrate')
+call_command('createsuperuser', interactive=False, username='admin', email='admin@example.com', password='admin')
+
 
 application = get_wsgi_application()
